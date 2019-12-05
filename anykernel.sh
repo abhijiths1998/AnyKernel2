d3839a9167c2c291b4124b2014eb0db1aa744f07
abhivarma362 <abhijiths0098@gmail.com>
2019-12-05 12:39:18 +0000

    Fix for magisk and kernel not recognising bug

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Matrix-Kernel by abhijiths362 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=beryllium
+device.name2=PocoF1
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -32,31 +32,40 @@ ramdisk_compression=auto;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 750 750 $ramdisk/init.qcom.rc $ramdisk/sbin;
 
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# use custom kernel compression
+kernel_compression=gzip;
+kernel_comp_ext=gz;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
+# combine kernel image and dtbs if separated in the zip
+decompressed_image=$home/kernel/Image;
+compressed_image=$decompressed_image.$kernel_comp_ext;
+combined_image=$home/Image.$kernel_comp_ext-dtb;
+if [ -f $compressed_image ]; then
+  # hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if ! $bin/magiskboot cpio $split_img/ramdisk.cpio test; then
+    ui_print " " "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=$kernel_compression $decompressed_image $compressed_image;
+  fi;
+  if [ -d $home/dtbs ]; then
+    cat $compressed_image $home/dtbs/*.dtb > $combined_image;
+  else
+    mv -f $compressed_image $home;
+  fi;
+fi;
 
 write_boot;
 ## end install
